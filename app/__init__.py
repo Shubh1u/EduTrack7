da1a70e0db1b5fa6a21db4472f30ab17a2bf20f4
Shubh1u <105773461+Shubh1u@users.noreply.github.com>
2025-09-03 09:09:21 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 from app.config import DevelopmentConfig
