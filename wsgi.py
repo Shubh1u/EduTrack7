8a042fe58e0c0179ecbdefdefbdd47414d954b84
Shubh1u <105773461+Shubh1u@users.noreply.github.com>
2025-09-03 09:13:17 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
-from app import create_app
+from app import create_app, socketio, db
 
 app = create_app()
+
+# ✅ Create tables at runtime
+with app.app_context():
+    db.create_all()
+    print("✅ Tables created at runtime")
+
+if __name__ == "__main__":
+    socketio.run(app)
